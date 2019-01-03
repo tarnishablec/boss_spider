4532a283e633fa1c694ab4ef8948e6d5c38bcc95
tarnishablec <tarnishablec@outlook.com>
2019-01-03 20:43:31 +0800

    write to mongodb

--- mongo_writer.py
+++ mongo_writer.py
@@ -7,6 +7,8 @@ db_spider = conn.spider
 
 spider = BossSpider('java', '南京', 1, 2)
 
-coll = db_spider.南京_java
+coll_name = spider.city + '_' + spider.keyword
+
+coll = db_spider[coll_name]
 
 coll.insert_many(spider.run())
